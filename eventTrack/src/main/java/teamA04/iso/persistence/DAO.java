e2df6125cf350806ba8a145872c7bc93ff02fb5c
Juan <Juan.Alcazar2@alu.uclm.es>
2023-11-25 23:26:13 +0100

    Implementation of UC2

---
+++ eventTrack/src/main/java/teamA04/iso/persistence/DAO.java
@@ -0,0 +1,16 @@
+package xd;
+
+public class DAO<T> {
+	public T read(String id) {
+		return null;
+	}
+	public boolean update (String id) {
+		return false;
+	}
+	public boolean delete(String id) {
+		return false;
+	}
+	public boolean add(T Event) {
+		return false;
+	}
+}
