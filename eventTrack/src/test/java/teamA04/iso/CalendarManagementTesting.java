be382fa787fcd9e50aab86946b54b8d53f453e99
Juan <Juan.Alcazar2@alu.uclm.es>
2023-12-01 15:13:43 +0100

    Testing of UC5

---
+++ eventTrack/src/test/java/teamA04/iso/CalendarManagementTesting.java
@@ -0,0 +1,21 @@
+package teamA04.iso;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CalendarManagementTesting {
+	
+	@Test
+	public void testViewAvailableEvents() {
+		boolean testViewAvailableEvents=true;
+		assertTrue(testViewAvailableEvents);
+	}
+	
+	@Test
+	public void testAddEventsToCalendar(){
+		boolean testAddEventsToCalendar;
+		assertTrue(testAddEventsToCalendar);
+	}
+	
+}
