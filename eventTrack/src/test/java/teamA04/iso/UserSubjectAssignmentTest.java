a7c9bc5d25c61f3be186b64c1b6c00e454584e07
Juan <Juan.Alcazar2@alu.uclm.es>
2023-12-04 19:34:19 +0100

    Testing of UC8

---
+++ eventTrack/src/test/java/teamA04/iso/UserSubjectAssignmentTest.java
@@ -0,0 +1,19 @@
+package teamA04.iso;
+
+import static org.junit.Assert.assertTrue;
+
+public class UserSubjectAssignmentTest {
+	
+	@Test
+	public void testStudentAssignment() {
+		boolean testStudentAssignment=true;
+		assertTrue(testStudentAssignment);
+	}
+
+	@Test
+	public void testTeacherAssignment() {
+		boolean testTeacherAssignment=true;
+		assertTrue(testTeacherAssignment);
+	}
+	
+}
