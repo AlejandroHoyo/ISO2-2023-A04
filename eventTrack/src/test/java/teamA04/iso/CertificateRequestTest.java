c5b07c5425a1924ed45e66b2201800baa6e7c11b
Juan <Juan.Alcazar2@alu.uclm.es>
2023-12-12 22:43:20 +0100

    Testing of UC10

---
+++ eventTrack/src/test/java/teamA04/iso/CertificateRequestTest.java
@@ -0,0 +1,13 @@
+package teamA04.iso;
+
+import static org.junit.Assert.assertTrue;
+
+public class CertificateRequestTest {
+
+	@Test
+	public void testRequestCertificate() {
+		boolean testRequestCertificate=true;
+		assertTrue(testRequestCertificate);
+	}
+	
+}
