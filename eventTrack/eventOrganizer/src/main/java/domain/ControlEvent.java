c72fb8fb63c6e933e3ff6d79ef510874a8ef6dae
adriangm16 <Adrian.Gomez14@alu.uclm.es>
2023-12-19 18:25:33 +0100

    Implemtation of UC12 in Organizer system

---
+++ eventTrack/eventOrganizer/src/main/java/domain/ControlEvent.java
@@ -0,0 +1,16 @@
+package Domain;
+
+public class ControlEvent {
+	public void editEvent(Event e) {
+	}
+	public Event displayEvent(String ID) {
+		Event e;
+		return e;
+	}
+	public void planEvent(Event e) {
+		
+	}
+	public void categorizeEvent(Event e) {
+		
+	}
+}
---
+++ eventTrack/eventOrganizer/src/main/java/presentation/CalendarInterface.java
@@ -0,0 +1,5 @@
+package xd;
+import javax.swing.JFrame;
+public class CalendarInterface extends JFrame{
+
+}
