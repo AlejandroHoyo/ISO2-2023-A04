776790feae83a5d986799991015913dc6f5a78ab
Juan <Juan.Alcazar2@alu.uclm.es>
2023-12-20 16:06:33 +0100

    Testing of UC12

---
+++ eventTrack/eventOrganizer/src/main/java/domain/OnlineEvent.java
@@ -0,0 +1,16 @@
+package teamA04.iso.domain;
+import teamA04.iso.Event;
+
+public class OnlineEvent {
+	
+	public long generateQrCode() {
+		
+		return qrCode;
+	}
+	
+	public int generatePinCode() {
+		
+		return pin;
+	}
+
+}
